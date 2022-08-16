14d09c765a725cf7784c1802b9f5a71ff706fdef
PROFESSOR-SAAB <109969024+PROFESSOR-SAAB@users.noreply.github.com>
2022-08-16 07:40:58 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -19,9 +19,12 @@ from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
 
-Hello {},My Name is {} !. 
+HELLO {},HOW ARE YOU {} !🌹,
 
-I'm Filter Manager Bot Maintained By [{}](https://t.me/{}). 
+I'm an advanced filter bot  [{}](https://t.me/{}). with many capabilities!
+Made by @PROFFASER_SAAB
+
+See /help for commands and more details.. 
 
 """
 
@@ -127,13 +130,13 @@ def start(bot: Bot, update: Update, args: List[str]):
         else:
             first_name = update.effective_user.first_name
             update.effective_message.reply_photo(DEVIL_IMG,PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_NAME, OWNER_USERNAME ),reply_markup=InlineKeyboardMarkup(
-                                                [[InlineKeyboardButton(text="📞Help",url="t.me/{}?start=help".format(bot.username)),InlineKeyboardButton(text=" 👥 channel.",url="https://telegram.dog/Mai_bOTs")],  
-                                                [InlineKeyboardButton(text="Creater",url="https://t.me/No_OnE_Kn0wS_Me"),InlineKeyboardButton(text="Mai Source",url="https://github.com/No-OnE-Kn0wS-Me/Filterbot")]]),disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
+                                                [[InlineKeyboardButton(text="📞Help",url="t.me/{}?start=help".format(bot.username)),InlineKeyboardButton(text=" 👥 channel.",url="https://t.me/+yuMu7Eg3jUlkY2M1")],  
+                                                [InlineKeyboardButton(text="Creater",url="https://t.me/No_OnE_Kn0wS_Me"),InlineKeyboardButton(text="Mai Source",url="https://github.com/PROFESSOR-SAAB/Filterbot")]]),disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
     else:
          
 
         update.effective_message.reply_text("Heya, How can I help you? 🙂",reply_markup=InlineKeyboardMarkup(
-                                                [[InlineKeyboardButton(text="❓ Help",url="t.me/{}?start=help".format(bot.username)),InlineKeyboardButton(text=" Mai Repo",url="https://github.com/No-OnE-Kn0wS-Me/Filterbot")]]))
+                                                [[InlineKeyboardButton(text="❓ Help",url="t.me/{}?start=help".format(bot.username)),InlineKeyboardButton(text=" Mai Repo",url="https://github.com/PROFESSOR-SAAB/Filterbot")]]))
                                   
 # for test purposes
 def error_callback(bot, update, error):
